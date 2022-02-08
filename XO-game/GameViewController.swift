e636844f57bc3121b4f3b07c71dd74295b774d8f
Sizov Denis <sv_skatch@bk.ru>
2022-02-08 12:32:01 +0300

    Added Changing player states logic

--- XO-game/GameViewController.swift
+++ XO-game/GameViewController.swift
@@ -15,17 +15,54 @@ class GameViewController: UIViewController {
     @IBOutlet var secondPlayerTurnLabel: UILabel!
     @IBOutlet var winnerLabel: UILabel!
     @IBOutlet var restartButton: UIButton!
+	
+	private let gameboard = Gameboard()
+	private var currentState: GameState! {
+		didSet {
+			self.currentState.begin()
+		}
+	}
     
     override func viewDidLoad() {
         super.viewDidLoad()
+		self.goToFirstState()
+		
         gameboardView.onSelectPosition = { [weak self] position in
             guard let self = self else { return }
             self.gameboardView.placeMarkView(XView(), at: position)
+			
+			if self.currentState.isCompleted {
+				self.goToNextState()
+			}
         }
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
         
     }
+	
+	// MARK: - Private
+	
+	/// Запускает первое состояние
+	private func goToFirstState() {
+		self.currentState = PlayerInputState(
+			player: .first,
+			gameViewController: self,
+			gameboard: gameboard,
+			gameboardView: gameboardView
+		)
+	}
+	
+	/// Переходит в следующее состояние со следующим игроком, если сейчас стостояние PlayerInputState
+	private func goToNextState() {
+		if let playerInputState = currentState as? PlayerInputState {
+			self.currentState = PlayerInputState(
+				player: playerInputState.player.next,
+				gameViewController: self,
+				gameboard: gameboard,
+				gameboardView: gameboardView
+			)
+		}
+	}
 }
 
